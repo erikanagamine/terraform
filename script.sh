57225b75837923f5bd93943f8425737bbfa6d1cc
Erika Nagamine <49041703+erikanagamine@users.noreply.github.com>
2020-08-26 20:40:53 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set +x
 
+#until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
+#  sleep 1
+#done
 
 # install nginx
 sudo apt-get update -y
@@ -14,7 +17,7 @@ cd /tmp/install
 
 git clone https://github.com/fabiogjardim/bigdata_docker.git
 
-sudo apt install x11-apps
+sudo apt install x11-apps -y
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 

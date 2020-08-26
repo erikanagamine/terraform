5b2c3a37f5aa1615de9d7d72b2be3f22ca758ced
Erika Nagamine <49041703+erikanagamine@users.noreply.github.com>
2020-08-26 03:57:53 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,4 +8,29 @@ sudo apt-get -y install nginx
 # make sure nginx is started
 sudo service nginx start
 
+mkdir /tmp/install
+
+cd /tmp/install
+
 git clone https://github.com/fabiogjardim/bigdata_docker.git
+
+sudo apt install x11-apps
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+curl -fsSL get.docker.com -o get.docker.sh
+
+sh -x get.docker.sh
+
+sudo usermod -aG docker ubuntu
+
+sudo docker version
+
+base=https://github.com/docker/machine/releases/download/v0.16.0 &&
+curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
+chmod +x /usr/local/bin/docker-machine
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
